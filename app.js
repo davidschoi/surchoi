cd78a7a16c7db25f5e39a7776877ce932c5d35b9
davidgumzchoi <davidgumzchoi@gmail.com>
2016-12-19 12:04:36 -0800

    rsvp form, our story, bridal party

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// AJAX Load Components
+// AJAX load components
 $('#navContainer').load('templates/nav.html');
 $('#mainContainer').load('partials/home.html', function() {
   // Resize home background to fill viewport
@@ -11,7 +11,7 @@ $('#mainContainer').load('partials/home.html', function() {
     $('#carousel').css({'max-height': windowHeight + 'px'});
   }
 
-  // jQuery for page scrolling feature - requires jQuery Easing plugin
+  // jQuery Easing page scrolling
   $('a.page-scroll').bind('click', function(event) {
     var $anchor = $(this);
     $('html, body').stop().animate({
@@ -20,22 +20,22 @@ $('#mainContainer').load('partials/home.html', function() {
     event.preventDefault();
   });
 
-  // Highlight the top nav as scrolling occurs
+  // Highlight the top nav during scrolling
   $('body').scrollspy({
     target: '.navbar-fixed-top',
   })
 
-  // Closes the Responsive Menu on Menu Item Click
+  // Closes responsive menu on menu item click
   $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
   });
 
-  // Closes Modal on Submit
+  // Closes modal on submit
   $('#modal [type="submit"]').on('click', function() {
     $('#modal .close').click();
   })
 
-  // Fit Text Plugin for Main Header
+  // Fit text plugin for main header
   $('h1').fitText(
     1.2, {
       minFontSize: '50px',
@@ -49,7 +49,7 @@ $('#mainContainer').load('partials/home.html', function() {
     }
   );
 
-  // Offset for Main Navigation
+  // Offset for main navigation
   $('#nav').affix({
     offset: {
       top: 100
@@ -58,3 +58,72 @@ $('#mainContainer').load('partials/home.html', function() {
 });
 $('#footerContainer').load('templates/footer.html');
 $('#modalContainer').load('templates/modal.html');
+
+$(document).ready(function() {
+  // Attending Text
+  var $buttonAttending = $('#modal .btn-success'),
+      $buttonNotAttending = $('#modal .btn-danger'),
+      $inputAttending = $('input[name="entry.519471964"]'),
+      $textAttending = $('#modal .text-success'),
+      $textNotAttending = $('#modal .text-danger'),
+      $anchorChangeResponse = $('.change-response');
+
+  $buttonAttending.click(function() {
+    $buttonAttending.addClass('hidden');
+    $buttonNotAttending.addClass('hidden');
+    $textAttending.removeClass('hidden');
+    $anchorChangeResponse.removeClass('hidden');
+    $inputAttending.val('yes');
+  });
+
+  $buttonNotAttending.click(function() {
+    $buttonNotAttending.addClass('hidden');
+    $buttonAttending.addClass('hidden');
+    $textNotAttending.removeClass('hidden');
+    $anchorChangeResponse.removeClass('hidden');
+    $inputAttending.val('no');
+  });
+
+  $anchorChangeResponse.click(function() {
+    $buttonAttending.removeClass('hidden');
+    $buttonNotAttending.removeClass('hidden');
+    $textAttending.addClass('hidden');
+    $anchorChangeResponse.addClass('hidden');
+    $textNotAttending.addClass('hidden');
+  });
+
+  // +1 condition display
+  var plusOneArray = [
+    "jasonchoi",
+    "jenniferlee",
+    "helenoh",
+    "juliepak",
+    "joshyoon","eunicepark","euniceyoon",
+    "derrickwu","margaretcho",
+    "danielpark","gloriasong","gloriapark",
+    "alexvareta","lysettevareta",
+    "cristeenkim","cristinelee","frankkim",
+    "mikelthomas",
+    "dianasearle",
+    "eugeneeum","jenniferlee",
+    "annasanders","annasanderslee","annalee","chenghoulee","cheng-houlee",
+    "andykwon","dianekwon","dianeluong","dianeluongkwon",
+    "ericcho",
+    "richardlee","hestialim","hestialee",
+    "philjung","hannahjung","hannahsheen",
+    "sophiecho","sophiekim","paulkim",
+    "michellehong","michellekim","jonathankim",
+    "richardpark","christineyun",
+    "davidkim","tammykim"
+  ];
+
+  var $inputName = $('input[name="entry.140860861"]'),
+      $inputPlusOneContainer = $('input[name="entry.1624099299"]').parents('.form-group');
+
+  $inputName.keyup(function() {
+    $inputPlusOneContainer.addClass('hidden');
+    if($.inArray(this.value.toLowerCase().replace(' ', ''), plusOneArray) > -1) {
+      $inputPlusOneContainer.removeClass('hidden');
+    }
+  });
+});
