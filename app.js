d895c9628153ee0cfb685b1df7fa39a2f14b2e7f
davidgumzchoi <davidgumzchoi@gmail.com>
2017-01-03 13:05:17 -0800

    change keyup bind function on rsvp form

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ $(document).ready(function() {
       $submitButton = $('.modal-footer .btn-primary[type="submit"]'),
       $errorMessage = $('.modal-footer .error-message');
 
-  $fields.keyup(function() {
+  $fields.bind("change keyup", function() {
     var $emptyFields = $fields.filter(function() {
       return $.trim(this.value) === '';
     });
